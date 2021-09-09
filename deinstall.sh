abe0f2d57a9b983418bb50ef1b74883b3c337cd0
Nicholas Melnick <nrmelnick@gmail.com>
2021-09-08 21:09:54 -0400

    Add separate deinstall tool

--- deinstall.sh
+++ deinstall.sh
@@ -58,5 +58,5 @@ ORDERING=("${LIBS[@]}" "${SESSION[@]}" "${APPS[@]}" "${PLUGS[@]}" "${THIRD_PARTY
 for p in "${ORDERING[@]}"; do
         cd "$R/$p"
         echo "=== $R/$p"
-        sudo make deinstall; sudo make clean && sudo make package && sudo make install
+        sudo make deinstall
 done
